5513379bb007035967c01b443246a634c3d0a77c
Ivan <>
2024-08-04 14:16:48 +0200

    Removed simple analytics. Added animation on the sticky bar and calculate when is pinned by using JS.

---
+++ javascripts/site.js
@@ -0,0 +1,14 @@
+const stickyElements = document.querySelectorAll('.sticky-bar');
+
+const onScroll = () => {
+  requestAnimationFrame(() => {
+    stickyElements.forEach((sticky) => {
+      if (sticky) {
+        const elementCSSTop = parseInt(window.getComputedStyle(sticky, null).getPropertyValue('top'));
+        sticky.classList.toggle('is-pinned', sticky.getBoundingClientRect().top === elementCSSTop);
+      }
+    });
+  });
+};
+
+window.addEventListener('scroll', onScroll);
