f53ffef86c22bb9b8e2e7489e03e7f495f62b123
Ivan <>
2024-08-04 14:23:21 +0200

    Removed requestAnimationFrame function.

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,13 +1,11 @@
 const stickyElements = document.querySelectorAll('.sticky-bar');
 
 const onScroll = () => {
-  requestAnimationFrame(() => {
-    stickyElements.forEach((sticky) => {
-      if (sticky) {
-        const elementCSSTop = parseInt(window.getComputedStyle(sticky, null).getPropertyValue('top'));
-        sticky.classList.toggle('is-pinned', sticky.getBoundingClientRect().top === elementCSSTop);
-      }
-    });
+  stickyElements.forEach((sticky) => {
+    if (sticky) {
+    const elementCSSTop = parseInt(window.getComputedStyle(sticky, null).getPropertyValue('top'));
+    sticky.classList.toggle('is-pinned', sticky.getBoundingClientRect().top === elementCSSTop);
+    }
   });
 };
 
